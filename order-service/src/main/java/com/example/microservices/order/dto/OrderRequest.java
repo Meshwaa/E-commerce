bec6bc7b20a0ea1e84ea6b5872603f6802b9248f
Meshwa <meshwa118@gmail.com>
2024-09-22 21:49:38 +0530

    Integrate Kafka with Schema Registry

--- order-service/src/main/java/com/example/microservices/order/dto/OrderRequest.java
+++ order-service/src/main/java/com/example/microservices/order/dto/OrderRequest.java
@@ -3,5 +3,7 @@ package com.example.microservices.order.dto;
 import java.math.BigDecimal;
 import java.util.List;
 
-public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity) {
+public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity,UserDetails userDetails) {
+    public record UserDetails(String email, String firstName, String lastName) {
+    }
 }
--- order-service/src/main/java/com/example/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/example/microservices/order/service/OrderService.java
@@ -2,23 +2,33 @@ package com.example.microservices.order.service;
 
 import com.example.microservices.order.client.InventoryClient;
 import com.example.microservices.order.dto.OrderRequest;
+import com.example.microservices.order.event.OrderPlacedEvent;
 import com.example.microservices.order.model.Order;
 import com.example.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
-    public void placeOrder(OrderRequest orderRequest){
-        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
+    private final KafkaTemplate<String,OrderPlacedEvent> kafkaTemplate;
 
-        if (isProductInStock){
+    public void placeOrder(OrderRequest orderRequest) {
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
             order.setPrice(orderRequest.price());
@@ -26,8 +36,19 @@ public class OrderService {
             order.setQuantity(orderRequest.quantity());
 
             orderRepository.save(order);
+
+            // Send message to kafka topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
+
+            log.info("Sending OrderPlacedEvent {} to Kafka topic order-placed.", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("Sent OrderPlacedEvent {} to Kafka topic order-placed.", orderPlacedEvent);
         } else {
-            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode()+ " is not in stock");
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
         }
     }
 }
